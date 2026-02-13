7f5ca19c3341338819fa4a62bbe6b1e7ec57b37a
Eriko1999 <86532439+Eriko1999@users.noreply.github.com>
2026-02-13 09:40:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,9 +35,9 @@ if ingredients_list:
             "insert into smoothies.public.orders(ingredients, name_on_order) values (?, ?)",
             params=[ingredients_string, name_on_order],
         ).collect()
-
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        
         st.success("Your Smoothie is ordered!", icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
